9c5207cc8919c71f9bf5a1bc2efc5e2612fcb35d
Harsh Vardhan Chauhan <harshvardhanc2697@gmail.com>
2025-08-21 16:19:21 +0530

    Create Villa Model

---
+++ HotelBooking.Domain/Entity/Villa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Domain.Entity
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string ?Name { get; set; }
+        public string ?Description { get; set; }
+        public string ?Price { get; set; }
+        public string ?Sqft { get; set; }
+        public string ?Occupency { get; set; }
+        public string ?ImageUrl { get; set; }
+        public DateTime CretedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
